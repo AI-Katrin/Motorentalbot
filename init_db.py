abae4b5ef3814497338c92fe0bfc58713551f3c3
AI-Katrin <katt.tri95@gmail.com>
2025-06-02 22:20:33 +0300

    total+price

---
+++ init_db.py
@@ -0,0 +1,6 @@
+from database import Base, engine  # импортируй то, что ты определила в database.py
+from models import Motorcycle  # важно: импортируй все модели, иначе они не создадутся
+
+Base.metadata.create_all(bind=engine)
+
+print("Таблицы успешно созданы.")
