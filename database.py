58d30b3291677e0fb880ec7a29073c96b43f1392
AI-Katrin <katt.tri95@gmail.com>
2025-06-02 21:59:11 +0300

    total+price

--- database.py
+++ database.py
@@ -1,7 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-
-SQLALCHEMY_DATABASE_URL = "postgresql://motorental_user:MotoRental2025@@localhost/motorental"
+from config import SQLALCHEMY_DATABASE_URL
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
